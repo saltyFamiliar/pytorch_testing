2153cdf3dd10cf160373858de6a3a9313730b1ea
cs <watchmaker3896@gmail.com>
2022-11-13 16:45:42 -0600

    fixed circular import with new config.py file

--- text_tut/data_proc.py
+++ text_tut/data_proc.py
@@ -6,25 +6,31 @@ from torchtext.datasets import AG_NEWS
 from torch.utils.data import DataLoader, random_split
 
 
-tokenizer = get_tokenizer('basic_english')
+ag_news_label = {1: "World",
+                 2: "Sports",
+                 3: "Business",
+                 4: "Sci/Tec"}
+
 train_iter, test_iter = AG_NEWS()
 train_dataset, test_dataset = to_map_style_dataset(train_iter), to_map_style_dataset(test_iter)
+num_class = len(set([label for (label, text) in train_iter]))
 num_train = int(len(train_dataset) * 0.95)
-split_train_, split_valid_ = random_split(train_dataset, [num_train, len(train_dataset) - num_train])
+num_test = len(train_dataset) - num_train
+split_train_, split_valid_ = random_split(train_dataset, [num_train, num_test])
 
-
+tokenizer = get_tokenizer('basic_english')
 def yield_tokens(data_iter):
     for _, text in data_iter: yield tokenizer(text)
 
-
 vocab = build_vocab_from_iterator(yield_tokens(train_iter), specials=["<unk>"])
 vocab.set_default_index(vocab["<unk>"])
+vocab_size = len(vocab)
 
 text_pipeline = lambda x: vocab(tokenizer(x))
 label_pipeline = lambda x: int(x) - 1
 
 def collate_batch(batch):
-    from main import device
+    from configs import device
     label_list, text_list, offsets = [], [], [0]
     for (_label, _text) in batch:
          label_list.append(label_pipeline(_label))
@@ -38,7 +44,6 @@ def collate_batch(batch):
 
 
 batch_size = 64
-dataloader = DataLoader(train_iter, batch_size=8, shuffle=False, collate_fn=collate_batch)
 train_dataloader = DataLoader(split_train_, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
 valid_dataloader = DataLoader(split_valid_, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
